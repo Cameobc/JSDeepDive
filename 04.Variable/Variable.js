e32264d3c6f48da61d6ce8caa43c23079919d463
Cameobc <dkqtk@naver.com>
2022-05-29 00:06:07 +0900

    #14장

--- 04.Variable/Variable.js
+++ 04.Variable/Variable.js
@@ -11,3 +11,23 @@ console.log(users);
 
 // 두 개 이상의 문자가 이어질 경우 카멜 케이스 추천
 let firstName;
+
+// 옵셔널 체이닝 연산자
+// 좌항의 피연산자가 null || undefined 인 경우 undefined 반환
+let elem = null;
+let value = elem?.value;
+console.log(value);
+
+value = elem && elem.value;
+console.log(value);
+
+let str = '';
+let length = str?.length;
+console.log(length);
+
+//null 병합 연산자
+// ?? 좌항의 피연산자가 null 또는 undefined 인 경우 우항의 피연산자를 반환
+let foo = null ?? 'default value';
+console.log(foo);
+foo = undefined ?? 'default valueeee';
+console.log(foo);
